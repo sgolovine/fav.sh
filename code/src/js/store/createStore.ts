9f2806efdeeeee3a86efc783bb134251c920c0a1
Sunny Golovine <sunny@glvn.co>
2020-02-14 19:17:26 -0500

    WIP Devtools + Persist

--- code/src/js/store/createStore.ts
+++ code/src/js/store/createStore.ts
@@ -1,11 +1,17 @@
 // General Imports
 import { combineReducers, createStore } from 'redux'
-
+import { persistStore, persistReducer } from 'redux-persist'
+import { localStorage } from 'redux-persist-webextension-storage'
 // Types
 import { AppState } from '../types/redux'
 
 // Inital State & Reducers
 import { initialState as moduleState, reducer as moduleReducer } from './module'
+
+const persistConfig = {
+  key: 'localStorage',
+  storage: localStorage,
+}
 
 const appInitialState: AppState = {
   module: moduleState,
@@ -14,5 +20,11 @@ const appInitialState: AppState = {
 const rootReducer = combineReducers({
   module: moduleReducer,
 })
+
+const persistedReducer = persistReducer(persistConfig, rootReducer)
+
+// TODO:
+// Finish store persistence
+// Finish devtools integration
 
 export const store = createStore(rootReducer, appInitialState)
